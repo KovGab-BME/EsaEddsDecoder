6e1fe50f42ad107dd68ded25816ef7abbeb140ac
Gabor Kovacs <kovgab@createch.hu>
2025-10-02 21:27:35 +0200

    PGM image nelkul

---
+++ EsaEddsDecoder/PgmImage.cpp
@@ -0,0 +1,26 @@
+﻿
+
+
+#include "PgmImage.h"
+#include <memory.h>
+#include <conio.h>
+#include <stdio.h>
+#include <string.h>
+
+CPgmImage::CPgmImage()
+{
+	m_width = 0;
+	m_height = 0;
+	m_nPixels = 0;
+	m_pPixels = NULL;
+}
+
+CPgmImage::~CPgmImage()
+{
+	if (m_pPixels != NULL) {
+		delete[] m_pPixels;
+		m_pPixels = NULL;
+	}
+
+}
+
